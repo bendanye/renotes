1208c2dfff8500d055593e4e8df373f0fc45033d
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2022-09-02 08:26:45 +0800

    Update random.sh

--- random.sh
+++ random.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+searchDir="./notes/tech"
+
+files=($searchDir/*)
+subdirectory_files=($searchDir/**/*)
+files+=(${subdirectory_files[@]})
+
+random_notes=$[$RANDOM % ${#files[@]}]
+selected_note=${files[random_notes]}
+
+echo "Open \"$selected_note\", read it!"
