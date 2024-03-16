659a3e9aa91c024218cb70bfd7525b980f5b6cc7
bendanye <shen_soonng@hotmail.com>
2024-03-16 14:02:45 +0800

    Output the random note in a log file with the date

--- open_random.sh
+++ open_random.sh
@@ -2,6 +2,14 @@ SCRIPT_DIR=$( dirname -- "$0"; )
 
 source $SCRIPT_DIR/random.sh
 
+function log() {
+    START=$(date +%s)
+    START_FORMATTED=$(date -r $START '+%Y-%m-%d %H:%M:%S')
+    echo "$START_FORMATTED,$1" > random.log
+}
+
 result=$(random $1)
+
+log $result
 
 open  -a "Google Chrome" $result
