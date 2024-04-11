41b3687bfefa91201338b06bc59a334b4595832c
bendanye <shen_soonng@hotmail.com>
2024-04-11 08:30:54 +0800

    Append random.log

--- open_random.sh
+++ open_random.sh
@@ -5,7 +5,7 @@ source $SCRIPT_DIR/random.sh
 function log() {
     START=$(date +%s)
     START_FORMATTED=$(date -r $START '+%Y-%m-%d %H:%M:%S')
-    echo "$START_FORMATTED,$1" > random.log
+    echo "$START_FORMATTED,$1" >> random.log
 }
 
 result=$(random $1)
