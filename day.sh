8fc5977fa7e42af934e1b6586cc46a56e0f21778
bendanye <shen_soonng@hotmail.com>
2024-05-18 13:57:47 +0800

    Send notes if is singapore public holiday or weekend

---
+++ day.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function isSingaporePublicHoliday() {
+  TODAY=$(date +%Y-%m-%d)
+
+  response=$(cat 2024_singapore_ph.json| jq --arg date "$TODAY" '[.[] | select(.date == $date)]')
+  if [ "$response" = "[]" ]; then
+    echo "false"
+  else
+    echo "true"
+  fi
+}
+
+function isWeekend() {
+  if [[ $(date +%u) -gt 5 ]]; then
+    echo "true"
+  else
+    echo "false"
+  fi
+}
--- print_random.sh
+++ print_random.sh
@@ -1,7 +1,14 @@
 SCRIPT_DIR=$( dirname -- "$0"; )
 
 source $SCRIPT_DIR/random.sh
+source $SCRIPT_DIR/day.sh
 
-result=$(random $1)
+isSingaporePublicHoliday=$(isSingaporePublicHoliday)
+isWeekend=$(isWeekend)
 
-echo "Open \"$result\" and read it!"
+if [[ $isSingaporePublicHoliday == "true" || $isWeekend == "true" ]]; then
+    result=$(random $1)
+    echo "Open \"$result\" and read it!"
+else
+    echo "DO_NOTHING"
+fi
